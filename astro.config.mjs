eb419418717de54a846e96e3fa4ab1f643b09595
Bryan P <bryanpsd@gmail.com>
2025-05-15 08:36:54 -0700

    Add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lilsammytrautmanssandbox.com/',
   integrations: [react()],
   adapter: netlify(),
   vite: {
