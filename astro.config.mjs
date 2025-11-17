79dba268ff07eb662c6fab8c95172ba7cc81774c
bryan <bryanpsd@gmail.com>
2025-11-17 12:37:30 -0800

    Update images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import mdx from '@astrojs/mdx';
 import netlify from '@astrojs/netlify';
+import partytown from '@astrojs/partytown';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { defineConfig } from 'astro/config';
-
-import partytown from '@astrojs/partytown';
-
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
